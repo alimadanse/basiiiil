0d10bce2aca246f3df31b729c44b201bea461d0b
alimadanse <133968085+alimadanse@users.noreply.github.com>
2023-05-26 17:34:27 +0330

    sex

---
+++ main/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.core.validators import RegexValidator
+from accounts.models import CustomUser
+
+from .models import Tour
+
+class TourForm(forms.ModelForm):
+    class Meta:
+        model = Tour
+        fields = ('title', 'description', 'photo')
+
+class TourRegisterForm(forms.ModelForm):
+    
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'nat_id', 'age', 'phone_number')
---
+++ main/views.py
@@ -0,0 +1,31 @@
+
+from django.shortcuts import render, get_object_or_404
+from .models import Tour
+from .forms import TourForm, TourRegisterForm
+from accounts.models import CustomUser
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class TourDetailView(generic.DetailView):
+    model = Tour
+    template_name = 'tour_detail.html'
+    context_object_name = "tour"
+
+
+class TourRegisterView(generic.UpdateView):
+    model = CustomUser     
+    form_class = TourRegisterForm
+    template_name = 'tour_register.html'
+    # context_object_name = "tour"    
+    success_url = reverse_lazy('home')
+
+
+class TourListView(generic.ListView):
+    model = Tour
+    template_name = 'tour_list.html'
+    context_object_name = 'tours'
+
+
+def home(request):
+    return render(request, 'home.html')
